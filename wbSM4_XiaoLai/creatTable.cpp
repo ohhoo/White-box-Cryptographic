9c2e766d03eda6f08054b8aaf77599a65bafe4a1
XuDong <Xudong1997124@outlook.com>
2020-11-01 20:35:32 +0800

    Update creatTable.cpp

--- wbSM4_XiaoLai/creatTable.cpp
+++ wbSM4_XiaoLai/creatTable.cpp
@@ -1,14 +1,14 @@
-//#include <NTL/vec_GF2.h>
-//#include <NTL/mat_GF2.h>
-//#include <iostream>
-//#include <fstream>
-//#include "generator.h"
-//
-//using namespace NTL;
-//
-//int main()
-//{
-//	uint mainKey[4] = { 0x01234567,0x89abcdef,0xfedcba98,0x76543210 };
-//	generator(mainKey);
-//	return 0;
-//}
+#include <NTL/vec_GF2.h>
+#include <NTL/mat_GF2.h>
+#include <iostream>
+#include <fstream>
+#include "generator.h"
+
+using namespace NTL;
+
+int main()
+{
+    uint mainKey[4] = { 0x01234567,0x89abcdef,0xfedcba98,0x76543210 };
+    generator(mainKey);
+    return 0;
+}
